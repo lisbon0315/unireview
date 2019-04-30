024578cfc89498f6360c0f63856f4093273db482
lisbon <lisbon0315666@gmail.com>
2019-04-30 16:58:59 +0900

    add-users_controller,sessions_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {in: 1..15}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  validates :password, length: {in: 8..32}, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+  validates :university, presence: true, length: {in: 1..12}
+
+  has_secure_password
+
   has_many :reviews
 end
