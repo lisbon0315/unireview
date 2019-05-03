db0307a654f7bf24799eae2d57fa8406fef1d270
lisbon <lisbon0315666@gmail.com>
2019-05-03 17:18:53 +0900

    add-before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login, only: %i[new create edit update]
+
+ private
+
+ def require_login
+   unless logged_in?
+     flash[:error] = "ログインしてください"
+     redirect_to new
+   end
+ end
 end
