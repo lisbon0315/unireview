477de8555d9fbebe84a3bee3b296125907ca13ce
lisbon <lisbon0315666@gmail.com>
2019-05-03 18:50:21 +0900

    change-lesson-controller,review-controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,16 +1,18 @@
 class LessonsController < ApplicationController
   before_action :require_login, only: %i[new create edit update]
+  PER = 20
 
   def index
-    @lessons = Lesson.all
+    @lessons = Lesson.page(params[:page]).per(PER)
   end
 
   def new
     @lesson = Lesson.new
+    @lesson.university_id = params[:university_id]
   end
 
   def create
-    @lesson = current_user.new(lesson_params)
+    @lesson = current_user.lessons.build(lesson_params)
 
     if @lesson.save
       redirect_to lessons_path, success: '投稿に成功しました'
@@ -22,13 +24,13 @@ class LessonsController < ApplicationController
 
   private
   def lesson_params
-    params.require(:lesson).permit(:name, :professor_name, :department)
+    params.require(:lesson).permit(:name, :professor_name, :department, :university_id)
   end
 
   def require_login
     unless logged_in?
       flash[:error] = "ログインしてください"
-      redirect_to user_new_path
+      redirect_to login_path
     end
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,18 @@
 class ReviewsController < ApplicationController
   before_action :require_login, only: %i[new create edit update]
-  
+  PER = 10
+
   def index
-    @reviews = Review.all
+    @reviews = Review.page(params[:page]).per(PER)
   end
 
   def new
     @review = Review.new
+    @review.lesson_id = params[:lesson_id]
   end
 
   def create
-    @review = current_user.new(review_params)
+    @review = current_user.reviews.build(review_params)
 
     if @review.save
       redirect_to reviews_path, success: '投稿に成功しました'
@@ -22,13 +24,13 @@ class ReviewsController < ApplicationController
 
   private
   def review_params
-    params.require(:review).permit(:content, :star)
+    params.require(:review).permit(:content, :star, :lesson_id)
   end
 
   def require_login
     unless logged_in?
       flash[:error] = "ログインしてください"
-      redirect_to user_new_path
+      redirect_to login_path
     end
   end
 end
